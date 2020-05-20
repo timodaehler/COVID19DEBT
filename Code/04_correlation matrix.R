05adf288ecafb4fa8a97ec163da934b02245e632
Timo Daehler <timo.daehler@gmail.com>
2020-05-20 20:48:06 +0200

    Committ before setting type of variables for dataframe that was extended with

---
+++ Code/04_correlation matrix.R
@@ -0,0 +1,20 @@
+#Author: Timo Daehler, daehler@usc.edu
+#Date of last update: 20 Mai 2020
+#Purpose: Correlation matrix and first couple of regressions after I completed the dataset
+#Inputs: 
+#Outputs: Figures in folder in folder "/Users/timodaehler/Desktop/COVID19DEBT/Plots/04"
+#Other relevant notes: 
+
+
+#loading required packages
+install.packages("tidyverse")
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(tibble)
+library(stringr)
+library(forcats)
+library(readxl)
+library(lubridate)
